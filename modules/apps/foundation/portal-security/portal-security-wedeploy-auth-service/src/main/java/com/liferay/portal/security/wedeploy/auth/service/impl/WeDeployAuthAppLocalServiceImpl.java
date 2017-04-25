1877e48103bec0e70e254af451a8b0b2a44c7548
suprithas <supritha.sundaram@liferay.com>
2017-04-25 15:07:54 -0700

    LPS-72151-pull-request - Implementation of token methods

--- modules/apps/foundation/portal-security/portal-security-wedeploy-auth-service/src/main/java/com/liferay/portal/security/wedeploy/auth/service/impl/WeDeployAuthAppLocalServiceImpl.java
+++ modules/apps/foundation/portal-security/portal-security-wedeploy-auth-service/src/main/java/com/liferay/portal/security/wedeploy/auth/service/impl/WeDeployAuthAppLocalServiceImpl.java
@@ -14,11 +14,136 @@
 
 package com.liferay.portal.security.wedeploy.auth.service.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.Digester;
+import com.liferay.portal.kernel.util.DigesterUtil;
+import com.liferay.portal.kernel.util.PwdGenerator;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+import com.liferay.portal.security.wedeploy.auth.model.WeDeployAuthApp;
 import com.liferay.portal.security.wedeploy.auth.service.base.WeDeployAuthAppLocalServiceBaseImpl;
+import com.liferay.portal.security.wedeploy.auth.service.persistence.WeDeployAuthAppPersistence;
+
+import java.util.Date;
 
 /**
+ * The implementation of the wedeploy auth app local service.
+ *
+ * <p>
+ * All custom service methods should be put in this class. Whenever methods
+ * are added, rerun ServiceBuilder to copy their definitions into the {@link
+ * com.liferay.portal.security.wedeploy.auth.service.WeDeployAuthAppLocalService
+ * } interface.
+ *
+ * <p>
+ * This is a local service. Methods of this service will not have security
+ * checks based on the propagated JAAS credentials because this service can
+ * only be accessed from within the same VM.
+ * </p>
+ *
  * @author Supritha Sundaram
+ * @see com.liferay.portal.security.wedeploy.auth.service.
+ * WeDeployAuthAppLocalServiceUtil
  */
 public class WeDeployAuthAppLocalServiceImpl
-	extends WeDeployAuthAppLocalServiceBaseImpl {
+		extends WeDeployAuthAppLocalServiceBaseImpl {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never reference this class directly. Always use {@link
+	 * com.liferay.portal.security.wedeploy.auth.service.
+	 * WedeployAuthAppLocalServiceUtil} to access the wedeploy auth app local
+	 * service.
+	 */
+
+	public WeDeployAuthApp addApplicationClient(
+			long userId, String name, long companyId,
+			ServiceContext serviceContext)
+			throws PortalException, SystemException {
+
+		User user = userPersistence.findByPrimaryKey(userId);
+		Date date = new Date();
+
+		long weDeployAuthAppId = counterLocalService.increment();
+
+		WeDeployAuthApp weDeployAuthApp = _weDeployAuthAppPersistence.create(
+				weDeployAuthAppId);
+
+		weDeployAuthApp.setCompanyId(user.getCompanyId());
+		weDeployAuthApp.setUserId(user.getUserId());
+		weDeployAuthApp.setUserName(user.getFullName());
+		weDeployAuthApp.setCreateDate(serviceContext.getCreateDate(date));
+		weDeployAuthApp.setModifiedDate(serviceContext.getModifiedDate(date));
+		weDeployAuthApp.setName(name);
+
+		String clientId = serviceContext.getUuid();
+
+		if (Validator.isNull(clientId)) {
+			clientId = PortalUUIDUtil.generate();
+		}
+
+		weDeployAuthApp.setClientId(clientId);
+
+		weDeployAuthApp.setClientSecret(randomizeToken(clientId));
+
+		_weDeployAuthAppPersistence.update(weDeployAuthApp, serviceContext);
+
+		// Resources
+
+		resourceLocalService.addModelResources(
+				weDeployAuthApp, serviceContext);
+
+		return weDeployAuthApp;
+	}
+
+	/**
+	 * Returns the authorization code.
+	 *
+	 * @return authorization code
+	 */
+	public String generateAuthorizationCode(String clientId)
+			throws PortalException, SystemException {
+
+		return _getAuthorizationCode(clientId);
+	}
+
+	/**
+	 * Returns the access token.
+	 *
+	 * @return access token
+	 */
+	public String generateAccessToken(String clientId, String clientSecret,
+									  String
+											  authorizationCode) {
+
+		WeDeployAuthApp weDeployAuthApp =
+				_weDeployAuthAppPersistence.fetchByClientId(clientId);
+
+		if (weDeployAuthApp == null) {
+			//generate client not found error
+		}
+
+		if (!clientSecret.equalsIgnoreCase(weDeployAuthApp.getClientSecret())) {
+			//generate secret does not match error
+		}
+
+		return randomizeToken(clientId.concat(authorizationCode));
+	}
+
+	private String _getAuthorizationCode(String clientId) {
+
+		String authorizationCode = randomizeToken(clientId);
+
+		return authorizationCode;
+	}
+
+	private String randomizeToken(String token) {
+		return DigesterUtil.digestHex(
+				Digester.MD5, token, PwdGenerator.getPassword());
+	}
+
+	private WeDeployAuthAppPersistence _weDeployAuthAppPersistence;
 }
